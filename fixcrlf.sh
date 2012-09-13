8dcb197a840e019d8b09efa2750ce5fa04a759da
Denis Ryabkov <d.v.ryabkov@gmail.com>
2012-09-13 10:26:51 +0600

    prepare files

---
+++ fixcrlf.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+IFS=$'\n'
+
+for file in $(find ./ -type f \( -name '*.js' -o -name '*.json' -o -name '*.java' -o -name '*.xml' -o -name '*.properties' \) ); do
+  perl -i -p -e 's/\r\n/\n/g' "$file"
+  perl -i -p -e 's/\r/\n/g' "$file"
+  perl -i -p -e 's/\t/    /g' "$file"
+done
+
